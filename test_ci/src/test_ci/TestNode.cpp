c263ce2a67895ecc572e9aeebb90a0d69ec9b747
Francisco Martin Rico <fmrico@gmail.com>
2021-02-17 08:37:49 +0100

    Test CI package
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

---
+++ test_ci/src/test_ci/TestNode.cpp
@@ -0,0 +1,45 @@
+// Copyright 2021 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "test_ci/TestNode.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "std_msgs/msg/string.hpp"
+
+namespace test_ci
+{
+
+using std::placeholders::_1;
+
+TestNode::TestNode()
+: Node("test_ci")
+{
+  sub_ = create_subscription<std_msgs::msg::String>("/message", 10, std::bind(&TestNode::string_callback, this, _1));
+}
+
+std::string
+TestNode::get_last_msg() const
+{
+  return last_msg_;
+}
+
+void 
+TestNode::string_callback(const std_msgs::msg::String::ConstSharedPtr msg)
+{
+  last_msg_ = msg->data;
+}
+
+
+}  // namespace plansys2
---
+++ test_ci/test/node_test.cpp
@@ -0,0 +1,75 @@
+// Copyright 2021 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "test_ci/TestNode.hpp"
+
+TEST(test_node, subscription)
+{
+  auto node = std::make_shared<test_ci::TestNode>();
+  
+  auto test_node = rclcpp::Node::make_shared("test_node");
+  auto test_pub = test_node->create_publisher<std_msgs::msg::String>(
+    "/message", 10);
+
+
+  rclcpp::executors::SingleThreadedExecutor exe;
+  exe.add_node(node);
+  exe.add_node(test_node);
+
+  bool finish = false;
+  std::thread t([&]() {
+      while (!finish) {exe.spin_some();}
+    });
+
+  ASSERT_EQ("", node->get_last_msg());
+
+  {
+    rclcpp::Rate rate(10);
+    auto start = test_node->now();
+    while ((test_node->now() - start).seconds() < 0.5) {
+      rate.sleep();
+    }
+  }
+
+  std_msgs::msg::String msg;
+  msg.data = "hello";
+  test_pub->publish(msg);
+
+  {
+    rclcpp::Rate rate(10);
+    auto start = test_node->now();
+    while ((test_node->now() - start).seconds() < 0.5) {
+      rate.sleep();
+    }
+  }
+
+  ASSERT_EQ("hello", node->get_last_msg());
+
+  finish = true;
+  t.join();
+}
+
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  rclcpp::init(argc, argv);
+
+  return RUN_ALL_TESTS();
+}
